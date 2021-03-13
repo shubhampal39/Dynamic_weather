1e156730355122911a0476b5c4a18617e651ccb8
unknown <shubhampal39@gmail.com>
2021-03-13 20:29:46 +0530

    Added partials and views

--- src/app.js
+++ src/app.js
@@ -1,20 +1,30 @@
 const express=require('express');
 const app = express();
+const hbs = require('hbs');
 const path=require('path');
 const port=process.env.PORT ||3000;
 
 const static_path =(path.join(__dirname,'../public'));
+const templates_path=(path.join(__dirname,'../templates/views'));
+const partials_path=(path.join(__dirname,'../templates/partials'));
 
-
+app.set('view engine','hbs'); 
+app.set('views',templates_path);
 app.use(express.static(static_path));
 
+//register partial views
+hbs.registerPartials(partials_path);
+
+app.get("",(req,res) => {
+    res.render('index')
+})
 
 app.get("/about",(req,res) => {
-    res.send("Welcome to about");
+    res.render('about')
 })
 
 app.get("/weather",(req,res) => {
-    res.send("Welcome to weather");
+    res.render('weather')
 })
 
 app.get("**",(req,res) => {
