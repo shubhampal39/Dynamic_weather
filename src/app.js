fee7e770321e617a93d0692b9e7b8e7dfa33d84f
unknown <shubhampal39@gmail.com>
2021-03-13 17:05:43 +0530

    Added css and html for weather app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express=require('express');
 const app = express();
+const path=require('path');
 const port=process.env.PORT ||3000;
 
-app.get("",(req,res) => {
-    res.send("Welcome to shubham pal");
-})
+const static_path =(path.join(__dirname,'../public'));
+
+
+app.use(express.static(static_path));
+
 
 app.get("/about",(req,res) => {
     res.send("Welcome to about");
