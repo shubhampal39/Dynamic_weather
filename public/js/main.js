c0ac504b9cfd6850cca458f7187f5fc6ce8ef68e
unknown <shubhampal39@gmail.com>
2021-03-14 09:12:34 +0530

    Added responsive code

--- public/js/main.js
+++ public/js/main.js
@@ -1,62 +1,84 @@
-// const cityName = document.getElementById('cityName');
-// const submitBtn = document.getElementById('submitBtn');
+const day = document.getElementById('day');
+const today_date=document.getElementById('today_date')
+// const month=document.getElementById('month');
+// const year=document.getElementById('year')
+const cityName = document.getElementById('cityName');
+const submitBtn = document.getElementById('submitBtn');
+const city_name = document.getElementById('city_name');
+const temp_real_val = document.getElementById('temp_real_val');
+const temp_status = document.getElementById('temp_status');
+const datahide = document.querySelector('.middle_layer');
 
-// const city_name = document.getElementById('city_name');
-// const temp_real_val = document.getElementById('temp_real_val');
-// const temp_status = document.getElementById('temp_status');
-// const datahide = document.querySelector('.middle_layer');
+const getInfo = async(event) => {
+    event.preventDefault();
 
-// const getInfo = async(event) => {
-//     event.preventDefault();
+    let cityVal = cityName.value;
 
-//     let cityVal = cityName.value;
+    if(cityVal === ""){
+        city_name.innerText = `Plz write the name before search`;
+        datahide.classList.add("data_hide");
+    }else{
 
-//     if(cityVal === ""){
-//         city_name.innerText = `Plz write the name before search`;
-//         datahide.classList.add("data_hide");
-//     }else{
-
-//         try{
+        try{
            
 
-//             let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=b14425a6554d189a2d7dc18a8e7d7263`
-//             const response = await fetch(url);
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=b14425a6554d189a2d7dc18a8e7d7263`
+            const response = await fetch(url);
 
-//             const data = await response.json();
-//             const arrData = [data];
+            const data = await response.json();
+            const arrData = [data];
 
-//             city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
-//             temp_real_val.innerText = arrData[0].main.temp;
-//             const tempMood = arrData[0].weather[0].main;
-//             console.log(tempMood);
+            city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
+            temp_real_val.innerText = arrData[0].main.temp;
+            const tempMood = arrData[0].weather[0].main;
+            console.log(tempMood);
 
-//             //condition to check sunny or cloudy
-//             if (tempMood == "Clear") {
-//             temp_status.innerHTML =
-//                 "<i class='fas  fa-sun' style='color: #eccc68;'></i>";
-//             } else if (tempMood == "Clouds") {
-//             temp_status.innerHTML =
-//                 "<i class='fas  fa-cloud' style='color: #f1f2f6;'></i>";
-//             } else if (tempMood == "Rain") {
-//             temp_status.innerHTML =
-//                 "<i class='fas  fa-cloud-rain' style='color: #a4b0be;'></i>";
-//             } else {
-//             temp_status.innerHTML =
-//                 "<i class='fas  fa-cloud' style='color:#f1f2f6;'></i>";
+            //condition to check sunny or cloudy
+            if (tempMood == "Clear") {
+            temp_status.innerHTML =
+                "<i class='fas  fa-sun' style='color: #eccc68;'></i>";
+            } else if (tempMood == "Clouds") {
+            temp_status.innerHTML =
+                "<i class='fas  fa-cloud' style='color: #f1f2f6;'></i>";
+            } else if (tempMood == "Rain") {
+            temp_status.innerHTML =
+                "<i class='fas  fa-cloud-rain' style='color: #a4b0be;'></i>";
+            } else {
+            temp_status.innerHTML =
+                "<i class='fas  fa-cloud' style='color:#f1f2f6;'></i>";
 
-//             }
-//             datahide.classList.remove('data_hide');
-//             cityVal = "";
+            }
+            datahide.classList.remove('data_hide');
+            cityVal = "";
            
        
-//         }catch{
-//             cityVal = " ";
-//             datahide.classList.add("data_hide");
-//             city_name.innerText =  `please enter the proper city name`;
-//             console.log('please add the proper city name');
-//         }
+        }catch{
+            cityVal = " ";
+            datahide.classList.add("data_hide");
+            city_name.innerText =  `please enter the proper city name`;
+            console.log('please add the proper city name');
+        }
         
-//     }
-// }
+    }
+}
 
-// submitBtn.addEventListener('click', getInfo);
+submitBtn.addEventListener('click', getInfo);
+
+// date and time
+
+var d = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
+day.innerText=n;
+// console.log(d.getDate());
+// console.log(d.getMonth() + 1);
+// console.log(d.getFullYear());
+today_date.innerText=`${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()}`;
